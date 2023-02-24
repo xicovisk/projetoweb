4493d889553686a777af3b3d0b0ae122ed0a33e9
Francisco <123470029+xicovisk@users.noreply.github.com>
2023-02-24 17:15:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.nav a[href^="#descricao"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
+
+
+  $('.nav a[href^="#servicos"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
